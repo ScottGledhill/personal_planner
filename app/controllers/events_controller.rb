586a80ae24d1d856cbe51baf80646e415457699f
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2017-01-18 17:09:51 +0000

    create method added

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,15 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def create
+    Event.create(event_params)
+    redirect_to 'index'
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:date, :time, :place, :event)
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ Bundler.require(*Rails.groups)
 
 module Planner
   class Application < Rails::Application
+    config.time_zone = "London"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
