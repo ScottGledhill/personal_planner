d65f949fa3e02032fc054d23a7154bffc9c9846d
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2017-01-18 17:31:17 +0000

    edit/destroy/update added

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,6 +8,26 @@ class EventsController < ApplicationController
     redirect_to 'index'
   end
 
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    @event.update_attributes(event_params)
+    redirect_to ('/')
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to ('/')
+  end
+
   private
 
   def event_params
