112692878d760916280684db67100dd8ecc12d79
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2017-01-25 19:50:02 +0000

    added uniqueness validation for attending

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,5 @@
 class Attendance < ApplicationRecord
+  validates :user, uniqueness: true
   belongs_to :event
   belongs_to :user
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -10,3 +10,13 @@ class Event < ApplicationRecord
   has_many :attendances # looks in the attendances table for all attendances with event_id == id
   has_many :users, through: :attendances # looks in the attendances returned from event.attendances, then finds all users with the id = attendance.user_id
 end
+
+# event.user_id == 9
+# user = event.user #<- the user method comes from the belongs_to class method. Calling this method looks in the db for the user with id == user_id
+# user.id = 9
+#
+#
+# event.id == 9
+# comments = event.comments
+# comment = comments.first
+# comment.event_id == 9§
