b73be86b62e430584a55cd48eb7c1eed481fcb09
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2017-01-31 18:36:31 +0000

    removed instance variables

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,17 +1,17 @@
 class AttendancesController < ApplicationController
   def create
-    @event = Event.find(params[:event_id])
-    @attendance = Attendance.new
-    @attendance.event = @event
-    @attendance.user = current_user
-    @attendance.save
-    redirect_to event_path(@event)
+    event = Event.find(params[:event_id])
+    attendance = Attendance.new
+    attendance.event = event
+    attendance.user = current_user
+    attendance.save
+    redirect_to event_path(event)
   end
 
   def destroy
-    @event = Event.find params[:event_id]
-    @attendance = @event.attendances.where(user_id: current_user.id).first
-    @attendance.destroy if @attendance.present?
-    redirect_to event_path(@event)
+    event = Event.find params[:event_id]
+    attendance = event.attendances.where(user_id: current_user.id).first
+    attendance.destroy if attendance.present?
+    redirect_to event_path(event)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
     resources :attendances, only: [:destroy, :create]
     resources :comments, shallow: true
   end
+  # resources :api do
+  #   resources :v1 do
+  #     resources :events
+  #   end
+  end
   # resources :users, only: [:show]
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 end
