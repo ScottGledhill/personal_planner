a67146b8869c0f1c868a827990c7428c1b9540aa
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2017-02-06 18:27:30 +0000

    user migration added

---
+++ db/migrate/20170118163111_fixdevise.rb
@@ -0,0 +1,18 @@
+class Fixdevise < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users,  :email, :string
+    add_column :users,  :encrypted_password, :string
+    add_column :users,  :reset_password_token, :string
+    add_column :users,  :reset_password_sent_at, :datetime
+    add_column :users,  :remember_created_at, :datetime
+    add_column :users,  :sign_in_count, :integer
+    add_column :users,  :current_sign_in_at, :datetime
+    add_column :users,  :last_sign_in_at, :datetime
+    add_column :users,  :current_sign_in_ip, :inet
+    add_column :users,  :last_sign_in_ip, :inet
+    add_column :users,  :created_at, :datetime
+    add_column :users,  :updated_at, :datetime
+    add_column :users,  :provider, :string
+    add_column :users,  :uid, :string
+  end
+end
