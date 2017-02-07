01bd33cb83eee90337fd227c886b9ed02780aba4
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2017-02-07 12:23:49 +0000

    proudction.rb

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,14 +1,23 @@
 class Api::V1::CommentsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  API_KEY = '123'
+
  def index
     render json: Event.find(params[:event_id]).comments
   end
 
   def create
+    return render plain: 'noooo' unless has_valid_api_key?
     event = Event.find(params[:event_id])
     comment = event.comments.new(comment_params)
     comment.save!
+  end
+
+  private
+
+  def has_valid_api_key?
+    request.headers['X-Api-Key'] == API_KEY
   end
 
   def comment_params
